6250085e70637a34fe072577c104791861c3ebbc
Michael Head <michaelehead@gmail.com>
2022-07-31 09:15:41 -0400

    fix test

--- test/my-device.test.ts
+++ test/my-device.test.ts
@@ -13,7 +13,7 @@ describe('MyDevice', () => {
   it('renders a h1', () => {
     const h1 = element.shadowRoot!.querySelector('h1')!;
     expect(h1).to.exist;
-    expect(h1.textContent).to.equal('My app');
+    expect(h1.textContent).to.equal('My Device');
   });
 
   it('passes the a11y audit', async () => {
